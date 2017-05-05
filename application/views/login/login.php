7d922f7d95484af6776c5680bc300745b0075042
cake <s5640216@gmail.com>
2017-05-05 16:14:11 +0800

    加入toastr套件

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -9,11 +9,14 @@
 		<!-- Bootstrap 3.3.6 -->
 		<link rel="stylesheet" href="<?=base_url('assets/bootstrap/css/bootstrap.min.css');?>">
 		<link rel="stylesheet" href="<?=base_url('assets/bootstrap/css/bootstrap-theme.min.css');?>">
+		
+		<link rel="stylesheet" href="<?=base_url('assets/toastr/toastr.min.css');?>">
 	</head>
 	<style>
-		body {
+		/*body {
 			padding-top: 90px;
-		}
+		}*/
+		
 		.panel-login {
 			border-color: #ccc;
 			-webkit-box-shadow: 0px 2px 3px 0px rgba(0,0,0,0.2);
@@ -110,86 +113,98 @@
 			background-color: #1CA347;
 			border-color: #1CA347;
 		}
-
+		.navbar-header {
+			float: left;
+			padding: 15px;
+			text-align: center;
+			width: 100%;
+		}
+		.navbar-brand {float:none;}
 	</style>
+	<nav class="navbar navbar-inverse" role="navigation">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#">外送茶</a>
+		</div>
+	</nav>
 	
 	<body>
 		
-	<div class="container">
-    	<div class="row">
-			<div class="col-md-6 col-md-offset-3">
-				<div class="panel panel-login">
-					<div class="panel-heading">
-						<div class="row">
-							<div class="col-xs-6">
-								<a href="#" class="active" id="login-form-link">登入</a>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3">
+					<div class="panel panel-login">
+						<div class="panel-heading">
+							<div class="row">
+								<div class="col-xs-6">
+									<a href="#" class="active" id="login-form-link">登入</a>
+								</div>
+								<div class="col-xs-6">
+									<a href="#" id="register-form-link">註冊</a>
+								</div>
 							</div>
-							<div class="col-xs-6">
-								<a href="#" id="register-form-link">註冊</a>
-							</div>
+							<hr>
 						</div>
-						<hr>
-					</div>
-					<div class="panel-body">
-						<div class="row">
-							<div class="col-lg-12">
-								<div id="login_form" style="display: block;">
-									<div class="form-group">
-										<input type="text" tabindex="1" class="account form-control" placeholder="帳號" value="">
-									</div>
-									<div class="form-group">
-										<input type="password" tabindex="2" class="passwd form-control" placeholder="密碼">
-									</div>
-									<!--<div class="form-group text-center">
-										<input type="checkbox" tabindex="3" class="" name="remember" id="remember">
-										<label for="remember"> Remember Me</label>
-									</div>-->
-									<div class="form-group">
-										<div class="row">
-											<div class="col-sm-6 col-sm-offset-3">
-												<button id="btnLogin" tabindex="4" class="form-control btn btn-login">登入</button>
+						<div class="panel-body">
+							<div class="row">
+								<div class="col-lg-12">
+									<div id="login_form" style="display: block;">
+										<div class="form-group">
+											<input type="text" tabindex="1" class="account form-control" placeholder="帳號" value="">
+										</div>
+										<div class="form-group">
+											<input type="password" tabindex="2" class="passwd form-control" placeholder="密碼">
+										</div>
+										<!--<div class="form-group text-center">
+											<input type="checkbox" tabindex="3" class="" name="remember" id="remember">
+											<label for="remember"> Remember Me</label>
+										</div>-->
+										<div class="form-group">
+											<div class="row">
+												<div class="col-sm-6 col-sm-offset-3">
+													<button id="btnLogin" tabindex="4" class="form-control btn btn-login">登入</button>
+												</div>
 											</div>
 										</div>
-									</div>
-									<div class="form-group">
-										<div class="row">
-											<div class="col-lg-12">
-												<div class="text-center">
-													<a href="#" tabindex="5" class="forgot-password">忘記密碼</a>
+										<div class="form-group">
+											<div class="row">
+												<div class="col-lg-12">
+													<div class="text-center">
+														<a href="#" tabindex="5" class="forgot-password">忘記密碼</a>
+													</div>
 												</div>
 											</div>
 										</div>
 									</div>
-								</div>
-								<div id="register_form" role="form" style="display: none;">
-									<div class="form-group">
-										<input type="text" tabindex="1" class="name form-control" placeholder="名稱" value="">
-									</div>
-									<div class="form-group">
-										<input type="text" tabindex="1" class="account form-control" placeholder="帳號" value="">
-									</div>
-									<div class="form-group">
-										<input type="password" tabindex="1" class="passwd form-control" placeholder="密碼">
-									</div>
-									<div class="form-group">
-										<input type="password" tabindex="1" class="passwd2 form-control" placeholder="確認密碼">
-									</div>
-									<div class="form-group">
-										<input type="email" tabindex="1" class="email form-control" placeholder="Email" value="">
-									</div>
-									<div class="form-group">
-										<label class="col-sm-2 control-label">性別</label>
-										<label class="radio-inline">
-											<input type="radio" name="sex" value="男" checked> 男
-										</label>
-										<label class="radio-inline">
-											<input type="radio" name="sex" value="女"> 女
-										</label>
-									</div>
-									<div class="form-group">
-										<div class="row">
-											<div class="col-sm-6 col-sm-offset-3">
-												<button tabindex="4" id="btnRegister" class="form-control btn btn-register">註冊</button>
+									<div id="register_form" role="form" style="display: none;">
+										<div class="form-group">
+											<input type="text" tabindex="1" class="name form-control" placeholder="名稱" value="">
+										</div>
+										<div class="form-group">
+											<input type="text" tabindex="1" class="account form-control" placeholder="帳號" value="">
+										</div>
+										<div class="form-group">
+											<input type="password" tabindex="1" class="passwd form-control" placeholder="密碼">
+										</div>
+										<div class="form-group">
+											<input type="password" tabindex="1" class="passwd2 form-control" placeholder="確認密碼">
+										</div>
+										<div class="form-group">
+											<input type="email" tabindex="1" class="email form-control" placeholder="Email" value="">
+										</div>
+										<div class="form-group">
+											<label class="col-sm-2 control-label">性別</label>
+											<label class="radio-inline">
+												<input type="radio" name="sex" value="男" checked> 男
+											</label>
+											<label class="radio-inline">
+												<input type="radio" name="sex" value="女"> 女
+											</label>
+										</div>
+										<div class="form-group">
+											<div class="row">
+												<div class="col-sm-6 col-sm-offset-3">
+													<button tabindex="4" id="btnRegister" class="form-control btn btn-register">註冊</button>
+												</div>
 											</div>
 										</div>
 									</div>
@@ -200,15 +215,17 @@
 				</div>
 			</div>
 		</div>
-	</div>
 		
 	</body>
 </html>
 
 <!-- jQuery 2.2.3 -->
-<script src="<?=base_url('assets/jquery/core/jquery-2.2.3.min.js');?>"></script>
+<script src="<?=base_url('assets/jquery/core/jquery-2.2.3.min.js');?>" type="text/javascript"></script>
 <!-- Bootstrap 3.3.6 -->
-<script src="<?=base_url('assets/bootstrap/js/bootstrap.min.js');?>"></script>
+<script src="<?=base_url('assets/bootstrap/js/bootstrap.min.js');?>" type="text/javascript"></script>
+
+<script src="<?=base_url('assets/toastr/toastr.min.js');?>" type="text/javascript"></script>
+<script src="<?=base_url('assets/common/common.js');?>" type="text/javascript"></script>
 
 <script>
 	$(function() {
@@ -249,9 +266,8 @@
 		}).done(function (data) {
 			if(data.status == "success"){
 				window.location.href = '<?=base_url();?>home';
-				//location.reload();
 			} else if(data.status == "fail"){
-				alert('帳號密碼有錯!');
+				error_toast(null ,'帳號密碼有錯!');
 			}
 		});
 	}
@@ -264,8 +280,7 @@
 		var email = $('#register_form .email').val();
 		var sex = $('input[name=sex]:checked').val();
 		var check_status = check(name,account,passwd,passwd2,email);
-		var email_status = check_email(email);
-		if(check_status == true && email_status == true){
+		if(check_status == true && check_email(email) == true){
 			$.ajax({
 				url: '<?=base_url()?>login/login/register',
 				type: 'POST',
@@ -280,10 +295,13 @@
 				},
 				success: function (res) {
 					if(res.status == "success"){
-						alert(res.message);
-						window.location = '<?=base_url()?>' + 'login/login';
+						success_toast('註冊資訊', res.message);
+						android.showToast(res.message);
+						$('#login-form-link').click();
+						$("#register_form input[type='text']").val('');
+						// window.location = '<?=base_url()?>' + 'login/login';
 					} else {
-						alert(res.message);
+						error_toast('註冊資訊', res.message);
 					}
 				}
 			});
@@ -294,33 +312,9 @@
 		if(name && account && passwd && passwd2 && email){
 			return true;
 		} else {
-			alert("表格不能為空");
+			warning_toast(null, '表格不能為空');
 			return false;
 		}
-		if(check_status == true && email_status == true){
-			$.ajax({
-				url: '<?=base_url()?>login/login/register',
-				type: 'POST',
-				dataType: 'json',
-				data: {
-					name: name,
-					account: account,
-					passwd: passwd,
-					passwd2: passwd2,
-					email: email,
-					sex: sex
-				},
-				success: function (res) {
-					if(res.status == "success"){
-						alert(res.message);
-						window.location = '<?=base_url()?>' + 'login';
-					} else {
-						alert(res.message);
-					}
-				}
-			});
-		}
-		
 	}
 	function check_email(email){
 		var pattern = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/; 
@@ -328,10 +322,12 @@
 		if(flag){
 			return true;
 		} else {
-			alert('請輸入正確的Email!!'); 
+			warning_toast(null, '請輸入正確的Email!!');
 			return false;
 		}
 	}
-		
+		function testf(){
+			
+		}
 </script>
 
