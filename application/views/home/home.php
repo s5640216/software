f50944fe5dd95df5cefc1a3f7c2703b5f5220dfe
wang <1103108109@gm.kuas.edu.tw>
2017-05-24 01:15:23 +0800

    將圖片丟上

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -9,12 +9,12 @@
     </header>
 
     <!-- About -->
-    <section id="about" class="about">
+    <section id="about" class="about bg2">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center" >
-                    <h1 class="detail">story</h1>
-                    <p class="lead">現今每個人都有空閒時間，透過共享經濟讓有空的人幫助需要幫助的人，不僅止於餐點也可以幫忙外送東西，只需要簡單小額便可以到所需，雙方得利，互取所需</p>
+                    <h1 class="text">story</h1>
+                    <p class="lead text">現今每個人都有空閒時間，透過共享經濟讓有空的人幫助需要幫助的人，不僅止於餐點也可以幫忙外送東西，只需要簡單小額便可以到所需，雙方得利，互取所需</p>
                 </div>
             </div>
             <!-- /.row -->
@@ -24,75 +24,7 @@
 
     <!-- Services -->
     <!-- The circle icons use Font Awesome's stacked icon classes. For more information, visit http://fontawesome.io/examples/ -->
-    <section id="services" class="services bg-primary">
-        <div class="container">
-            <div class="row text-center">
-                <div class="col-lg-10 col-lg-offset-1">
-                    <h2>Our Services</h2>
-                    <hr class="small">
-                    <div class="row">
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                <i class="fa fa-circle fa-stack-2x"></i>
-                                <i class="fa fa-cloud fa-stack-1x text-primary"></i>
-                            </span>
-                                <h4>
-                                    <strong>Service Name</strong>
-                                </h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                <a href="#" class="btn btn-light">Learn More</a>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                <i class="fa fa-circle fa-stack-2x"></i>
-                                <i class="fa fa-compass fa-stack-1x text-primary"></i>
-                            </span>
-                                <h4>
-                                    <strong>Service Name</strong>
-                                </h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                <a href="#" class="btn btn-light">Learn More</a>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                <i class="fa fa-circle fa-stack-2x"></i>
-                                <i class="fa fa-flask fa-stack-1x text-primary"></i>
-                            </span>
-                                <h4>
-                                    <strong>Service Name</strong>
-                                </h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                <a href="#" class="btn btn-light">Learn More</a>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                <i class="fa fa-circle fa-stack-2x"></i>
-                                <i class="fa fa-shield fa-stack-1x text-primary"></i>
-                            </span>
-                                <h4>
-                                    <strong>Service Name</strong>
-                                </h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                <a href="#" class="btn btn-light">Learn More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- /.row (nested) -->
-                </div>
-                <!-- /.col-lg-10 -->
-            </div>
-            <!-- /.row -->
-        </div>
-        <!-- /.container -->
-    </section>
-
+    
     <!-- Callout -->
     <aside class="callout">
         <div class="text-vertical-center">
@@ -101,16 +33,16 @@
     </aside>
 
     <!-- Portfolio -->
-    <section id="portfolio" class="portfolio">
-        <div class="container">
+    <section id="portfolio" class="portfolio bg">
+        <div class="container ">
             <div class="row">
                 <div class="col-lg-10 col-lg-offset-1 text-center">
-                    <h2>Our Work</h2>
+                    <h2 class="text">Our Work</h2>
                     <hr class="small">
                     <div class="row">
                         <div class="col-md-6">
                             <div class="portfolio-item">
-							 <h2>Deliver</h2>
+							 <h2 class="text">Deliver</h2>
                                 <a href="#">
                                     <img class="img-portfolio img-responsive" src="<?=base_url('assets/home/img/deliver.jpg');?>">
                                 </a>
@@ -118,7 +50,7 @@
                         </div>
                         <div class="col-md-6">
                             <div class="portfolio-item">
-							 <h2>Shop</h2>
+							 <h2 class="text">Shop</h2>
                                 <a href="#">
                                     <img class="img-portfolio img-responsive" src="<?=base_url('assets/home/img/shop.jpg');?>">
                                 </a>
@@ -126,7 +58,7 @@
                         </div>
                         <div class="col-md-6">
                             <div class="portfolio-item">
-							 <h2>Convenient</h2>
+							 <h2 class="text">Convenient</h2>
                                 <a href="#">
                                     <img class="img-portfolio img-responsive" src="<?=base_url('assets/home/img/busy.jpg');?>">
                                 </a>
@@ -134,7 +66,7 @@
                         </div>
                         <div class="col-md-6">
                             <div class="portfolio-item">
-							 <h2>Mutual benefit</h2>
+							 <h2 class="text">Mutual benefit</h2>
                                 <a href="#">
                                     <img class="img-portfolio img-responsive" src="<?=base_url('assets/home/img/hand.jpg');?>">
                                 </a>
@@ -152,30 +84,14 @@
     </section>
 
     <!-- Call to Action -->
-    <aside class="call-to-action bg-primary">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h3>The buttons below are impossible to resist.</h3>
-                    <a href="#" class="btn btn-lg btn-light">Click Me!</a>
-                    <a href="#" class="btn btn-lg btn-dark">Look at Me!</a>
-                </div>
-            </div>
-        </div>
-    </aside>
+    
 
     <!-- Map -->
-    <section id="contact" class="map">
-        <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
-        <br />
-        <small>
-            <a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A"></a>
-        </small>
-    </section>
+    
 
     <!-- Footer -->
     <footer>
-        <div class="container">
+        <div class="container footer-bg">
             <div class="row">
                 <div class="col-lg-10 col-lg-offset-1 text-center">
                     <h4><strong>Start Bootstrap</strong>
